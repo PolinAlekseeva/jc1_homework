565eda7ff2a0a610dd2a3fed89a3a6df6b135669
PolinAlekseeva <b.poline@gmail.com>
2022-02-28 14:12:25 +0300

    exercise 6

---
+++ src/my/tasks3_11/Home.java
@@ -0,0 +1,21 @@
+package my.tasks3_11;
+
+public class Home {
+    public static void main(String[] args) {
+        System.out.println(isSeven());
+    }
+
+        static boolean isSeven () {
+
+        int a = 38;
+            if (a % 10 == 7) {
+                return true;
+
+            } else {
+                return false;
+
+            }
+        }
+    }
+
+
