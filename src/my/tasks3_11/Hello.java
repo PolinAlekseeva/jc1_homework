d24d3b8b8dc70b8530a87c95bec4e3f313764ef7
PolinAlekseeva <b.poline@gmail.com>
2022-02-28 10:45:09 +0300

    exercise 3, p.19

---
+++ src/my/tasks3_11/Hello.java
@@ -0,0 +1,26 @@
+package my.tasks3_11;
+
+public class Hello {
+    public static void main(String[] args) {
+
+
+        String b = "0x55";
+        System.out.println("byte b = " + b + ";");
+        String s = "0x55ff";
+        System.out.println("short s = " + s + ";");
+        int i = 1000000;
+        System.out.println("int i = " + i + ";");
+        String l = "0xffffffffL";
+        System.out.println("long l = " + l + ";");
+        char c = 'a';
+        System.out.println("char c = " + c + ";");
+        String f = ".25f";
+        System.out.println("float f = " + f + ";");
+        String d = ".00001234";
+        System.out.println("double d = " + d + ";");
+        boolean bool = true;
+        System.out.println("boolean bool = " + bool + ";");
+
+
+    }
+}
---
+++ src/my/tasks3_11/HelloWorld.java
@@ -0,0 +1,23 @@
+package my.tasks3_11;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        byte b1 = 0x55;
+        System.out.println("byte b = " + b1 + ";");
+        short s1 = 0x55ff;
+        System.out.println("short s = " + s1 + ";");
+        int i1 = 1000000;
+        System.out.println("int i = " + i1 + ";");
+        long l1 = 0xffffffffL;
+        System.out.println("long l = " + l1 + ";");
+        char c1 = 'a';
+        System.out.println("char c = " + c1 + ";");
+        float f1 = .25f;
+        System.out.println("float f = " + f1 + ";");
+        double d1 = .00001234;
+        System.out.println("double d = " + d1 + ";");
+        boolean bool1 = true;
+        System.out.println("boolean bool = " + bool1 + ";");
+
+    }
+}
