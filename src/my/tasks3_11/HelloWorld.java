77b9170958ad5a4e6864cee355e6375c39db1f9b
PolinAlekseeva <b.poline@gmail.com>
2022-02-28 16:30:34 +0300

    exercise 3,p.19

--- src/my/tasks3_11/HelloWorld.java
+++ src/my/tasks3_11/HelloWorld.java
@@ -2,21 +2,23 @@ package my.tasks3_11;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         byte b = 0x55;
-        System.out.println("byte b = " + b + ";");
         short s = 0x55ff;
-        System.out.println("short s = " + s + ";");
         int i = 1000000;
-        System.out.println("int i = " + i + ";");
         long l = 0xffffffffL;
-        System.out.println("long l = " + l + ";");
         char c = 'a';
-        System.out.println("char c = " + c + ";");
         float f = .25f;
-        System.out.println("float f = " + f + ";");
         double d = .00001234;
-        System.out.println("double d = " + d + ";");
         boolean bool = true;
+
+        System.out.println("byte b = " + b + ";");
+        System.out.println("short s = " + s + ";");
+        System.out.println("int i = " + i + ";");
+        System.out.println("long l = " + l + ";");
+        System.out.println("char c = " + c + ";");
+        System.out.println("float f = " + f + ";");
+        System.out.println("double d = " + d + ";");
         System.out.println("boolean bool = " + bool + ";");
 
     }
