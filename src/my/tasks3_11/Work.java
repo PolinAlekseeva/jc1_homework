9d56198876a6db403e3ec430813097e48e3aabf1
PolinAlekseeva <b.poline@gmail.com>
2022-02-28 11:21:15 +0300

    exercise 4, p.20

---
+++ src/my/tasks3_11/Work.java
@@ -0,0 +1,25 @@
+package my.tasks3_11;
+
+public class Work {
+    public static void main(String[] args) {
+
+
+        Boxes myBoxes = new Boxes();
+        System.out.println("Результат: " + new Boxes().getRes1());
+
+
+    }
+}
+class Boxes {
+    int num1 = 5;
+    int num2 = 10;
+    int res1 = (num1 + num2) + (num1 * num2);
+
+    int getRes1() {
+        return res1;
+    }
+}
+
+
+
+
