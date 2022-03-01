87dbb26ace3e11806e4a2857432a0f8b21306205
PolinAlekseeva <b.poline@gmail.com>
2022-03-01 20:21:27 +0300

    exercise 8

--- src/my/tasks3_11/Money.java
+++ src/my/tasks3_11/Money.java
@@ -3,12 +3,13 @@ package my.tasks3_11;
 public class Money {
     public static void main(String[] args) {
 
-        int summa = 3200;
-        if (summa < 3200) {
-            System.out.println(summa + " рубли");
-        } else if (summa > 3200) {
+        int summa = 505;
+
+        if (summa % 10 == 1) {
             System.out.println(summa + " рубль");
-        } else if (summa >= 3200) {
+        } else if (summa % 10 == 2 || summa % 10 == 3 || summa % 10 == 4) {
+            System.out.println(summa + " рубля");
+        } else  {
             System.out.println(summa + " рублей");
         }
     }
