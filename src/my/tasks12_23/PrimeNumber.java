ddcd9690ba56168a300ee81cfd8b78d8c9044225
PolinAlekseeva <b.poline@gmail.com>
2022-03-09 10:43:11 +0300

    exercise 15, found second prime number

--- src/my/tasks12_23/PrimeNumber.java
+++ src/my/tasks12_23/PrimeNumber.java
@@ -2,14 +2,13 @@ package my.tasks12_23;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int a;
-        int b;
+
         boolean bool = true;
         boolean second = false;
-        for (a = 50; a <= 70; a++) {
+        for (int a = 50; a <= 70; a++) {
             bool = true;
 
-            for (b = 2; b < a; b++) {
+            for (int b = 2; b < a; b++) {
                 if (a % b == 0) {
                     bool = false;
                     break;
