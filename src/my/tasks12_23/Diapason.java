a8b0709683a6c31ff96e8228cad545e38a89198a
PolinAlekseeva <b.poline@gmail.com>
2022-03-09 12:39:41 +0300

    exercise 16, Hope

---
+++ src/my/tasks12_23/Diapason.java
@@ -0,0 +1,16 @@
+package my.tasks12_23;
+
+public class Diapason {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 7 == 0) {
+                System.out.println("Hope");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+}
