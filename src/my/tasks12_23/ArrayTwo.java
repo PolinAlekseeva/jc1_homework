4374dc53c340241f33cc3ce88da8fbcf1d61137c
PolinAlekseeva <b.poline@gmail.com>
2022-03-10 11:22:29 +0300

    exercise 19, create array and find elements with even index

--- src/my/tasks12_23/ArrayTwo.java
+++ src/my/tasks12_23/ArrayTwo.java
@@ -4,7 +4,7 @@ public class ArrayTwo {
 
     public static void main(String[] args) {
 
-        float[] nums = {6.5f, 3.2f, 7.08f, 25.9f, 5.5f, 7.3f, 98.5f, 103.6f, 87.4f, 9.7f };
+        int[] nums = {6, 3, 7, 25, 5, 7, 98, 103, 87, 9};
         for(int i = 0; i< nums.length; i++) {
             if ( i% 2 == 0) {
                 System.out.println(nums[i]);
