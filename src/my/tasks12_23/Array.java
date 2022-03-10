730c3066dea1267cc701898783b2cb41ade8835e
PolinAlekseeva <b.poline@gmail.com>
2022-03-10 10:33:43 +0300

    exercise 18, create array

---
+++ src/my/tasks12_23/Array.java
@@ -0,0 +1,21 @@
+package my.tasks12_23;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int [10];
+        nums [0] =5;
+        nums [1] = 3;
+        nums [2] = 28;
+        nums [3] = 17;
+        nums [4] = 35;
+        nums [5] = 415;
+        nums [6] = 8;
+        nums [7] = 98;
+        nums [8] = 65;
+        nums [9] = 73;
+
+        System.out.println(nums[9]);
+    }
+}
