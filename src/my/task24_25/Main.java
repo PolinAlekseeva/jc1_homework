3168db8a3e33a38e0e1893136c7cbcef1034c0e7
PolinAlekseeva <b.poline@gmail.com>
2022-03-16 13:53:58 +0300

    exercise 24, create two constructors

--- src/my/task24_25/Main.java
+++ src/my/task24_25/Main.java
@@ -3,12 +3,10 @@ package my.task24_25;
 public class Main {
     public static void main(String[] args) {
 
-        Time time = new Time(35000);
-        Time time2 = new Time(70000,350, 7);
+        Time time1 = new Time(35000);
+        Time time2 = new Time(50, 57, 7);
 
-        System.out.println("There are " + new Time(35000).getSec() + " seconds.");
-        System.out.println("All seconds are " + new Time(70000,350, 7).getTime());
-
+        System.out.println("The object time2 has " + time2.getSec() + " seconds.");
     }
 
 }
--- src/my/task24_25/Time.java
+++ src/my/task24_25/Time.java
@@ -1,33 +1,27 @@
 package my.task24_25;
 
 public class Time {
+
     int sec;
     int min;
     int hour;
 
+    public Time(int sec) {
+        this.sec = sec;
+        System.out.println("Object time1 has " + this.sec + " seconds");
+    }
 
-    public Time ( int s){
-
-        sec = s;
+    public Time(int s, int m, int h) {
+        this.sec = s;
+        this.min = m;
+        this.hour = h;
+        System.out.println("Object time2 has " + this.hour + " hours " + this.min + " minutes " + this.sec + " seconds");
 
     }
 
-    int getSec(){
-        return sec;
+    int getSec() {
+        return hour * 3600 + min * 60 + sec;
 
-    }
-
-    public Time (int s, int m, int h) {
-
-        sec = s;
-        min = m;
-        hour = h;
-
-    }
-
-    int getTime(){
-
-        return hour * 3600 + min*60 + sec;
     }
 
 }
