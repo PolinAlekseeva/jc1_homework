e9f6fdbdc0d82cd281169b513e2b160bd033905b
PolinAlekseeva <b.poline@gmail.com>
2022-03-17 12:15:48 +0300

    exercise 24, create constructors

--- src/my/task24_25/Main.java
+++ src/my/task24_25/Main.java
@@ -6,6 +6,15 @@ public class Main {
         Time time1 = new Time(35000);
         Time time2 = new Time(50, 57, 7);
 
-        System.out.println("The object time2 has " + time2.getSec() + " seconds.");
+
+        System.out.println("The object2 has " + time2.getSec() + " seconds.");
+
+        if (time1.equals(time2)){
+            System.out.println("The object1 is equal to object2.");
+        }
+        else{
+            System.out.println("The object1 is not equal to object2.");
+        }
     }
 }
+
--- src/my/task24_25/Time.java
+++ src/my/task24_25/Time.java
@@ -8,19 +8,28 @@ public class Time {
 
     public Time(int sec) {
         this.sec = sec;
-        System.out.println("Object time1 has " + this.sec + " seconds");
+        System.out.println("Object1 has " + this.sec + " seconds");
+
+        this.hour = sec / 3600 % 24;
+        sec %= 3600;
+        this.min = sec / 60;
+        this.sec = sec % 60;
+        System.out.println("Object1 has " + this.hour + " Hours " + this.min + " Minutes " + this.sec + " seconds");
+
     }
 
     public Time(int s, int m, int h) {
         this.sec = s;
         this.min = m;
         this.hour = h;
-        System.out.println("Object time2 has " + this.hour + " hours " + this.min + " minutes " + this.sec + " seconds");
+
+        System.out.println("Object2 has " + this.hour + " hours " + this.min + " minutes " + this.sec + " seconds");
 
     }
 
     int getSec() {
-        return hour * 3600 + min * 60 + sec;
+        return this.hour * 3600 + this.min * 60 + this.sec;
 
     }
 }
+
