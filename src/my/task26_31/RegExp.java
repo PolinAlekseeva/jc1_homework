7280b60e4597183fcc54b154362a0da4a67e899e
PolinAlekseeva <b.poline@gmail.com>
2022-03-28 13:15:29 +0300

    exercise 26,  count punctuation characters

--- src/my/task26_31/RegExp.java
+++ src/my/task26_31/RegExp.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class RegExp {
     public static void main(String[] args) {
+
         int count = 0;
         Pattern p = Pattern.compile("\\p{Punct}");
         Matcher m = p.matcher("What a beautiful day is today!! Learning a foreign language gives you chances to appreciate a new literature, a different culture!");
