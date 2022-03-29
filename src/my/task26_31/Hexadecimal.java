50eca39311476bf2adda0c205bcf679b7555edfa
PolinAlekseeva <b.poline@gmail.com>
2022-03-29 13:39:22 +0300

    exercise 29, regular expression to find Hexadecimal number

--- src/my/task26_31/Hexadecimal.java
+++ src/my/task26_31/Hexadecimal.java
@@ -1,8 +1,15 @@
 package my.task26_31;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Hexadecimal {
     public static void main(String[] args) {
-        String s1 = "0x5AFE";
-        System.out.println(s1.matches("0x[0-9a-fA-F]{1,4}"));
+        Pattern p = Pattern.compile("0[xX][0-9A-Fa-f]+");
+        Matcher m = p.matcher("0X5a3miihy some text needed here 1235, 45654, 0xfaee");
+        while (m.find()) {
+            System.out.println("Hexadecimal number is " + m.group() + " ");
+
+        }
     }
 }
